f9d5c98aaf46001191fc812746d587855a11837d
irek02 <imirgaleev@gmail.com>
2019-05-09 19:41:44 -0400

    end of getting started code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,10 +18,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes)
-  ],
-  exports: [RouterModule],
-  providers: []
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
