3f81f93ac96887d9771ee3dd1d5a6f32a908b03e
irek02 <imirgaleev@gmail.com>
2019-05-09 20:34:41 -0400

    Revert "add components"
    This reverts commit 2a98152948b4b33f44ae4828950292a3cad13f8e.

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderContainerComponent } from './containers/header-container/header-container.component';
-import { NavComponent } from './components/nav/nav.component';
-import { FiltersComponent } from './components/filters/filters.component';
 
 @NgModule({
-  declarations: [HeaderContainerComponent, NavComponent, FiltersComponent],
+  declarations: [HeaderContainerComponent],
   imports: [
     CommonModule
   ],
