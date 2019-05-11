9e903e3b4c8ffadce1d7428c53757b70359351a8
irek02 <imirgaleev@gmail.com>
2019-05-10 21:02:53 -0400

    add type and interface

--- src/app/modules/homes/containers/home-list-container/home-list-container.component.ts
+++ src/app/modules/homes/containers/home-list-container/home-list-container.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { HomeTypes } from 'src/app/modules/core/containers/header-container/header-container.component';
+
+export interface Home {
+  type: HomeTypes;
+  title: string;
+  location: string;
+  price: number;
+  image_url: string;
+  rating: {
+    count: number;
+    stars: number;
+  };
+}
 
 @Component({
   selector: 'app-home-list-container',
